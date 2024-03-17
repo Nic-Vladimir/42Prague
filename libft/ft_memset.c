423e6cd5f175abad224bec51d037b89daebb4e48
Nic-Vladimir <vladimirnicolescu2000@gmail.com>
2024-03-17 16:46:52 +0100

    fixed ft_memset

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -1,28 +1,14 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: vnicoles <vnicoles@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/14 13:15:34 by vnicoles          #+#    #+#             */
-/*   Updated: 2024/02/14 13:15:34 by vnicoles         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <unistd.h>
 
-void	*ft_memset(void *s, int c, size_t n)
-{
-	unsigned char	*p;
-	unsigned char	uc;
+void *ft_memset(void *s, int c, size_t n) {
+unsigned char *p;
+unsigned char uc;
 
-	*p = s;
-	uc = c;
-	while (n > 0)
-	{
-		*p++ = uc;
-		n--;
-	}
-	return (s);
+    *p = s;
+    uc = c;
+    while (n > 0) {
+        *p++ = uc;
+        n--;
+    }
+    return s;
 }
