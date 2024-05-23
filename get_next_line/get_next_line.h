e75433ca3b9a9d52a80e15411d6c712aec1dd290
Nic-Vladimir <vladimirnicolescu2000@gmail.com>
2024-05-23 11:17:16 +0200

    get_next_line V01

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vnicoles <vnicoles@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/22 23:21:37 by vnicoles          #+#    #+#             */
+/*   Updated: 2024/05/23 09:55:47 by vnicoles         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+char		*get_next_line(int fd);
+char		*ft_strjoin(const char *s1, const char *s2);
+char		*ft_substr(const char *s, unsigned int start, size_t len);
+char		*ft_strchr(const char *s, int c);
+char		*ft_strdup(const char *s1);
+size_t		ft_strlen(const char *a);
+
+#endif
