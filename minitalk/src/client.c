58340233ad9069c05afad3953bb14ef8297a971b
Vladimir Nicolescu <vnicoles@c2r10s3.42prague.com>
2025-01-03 06:37:38 +0100

    minitalk v1

--- minitalk/src/client.c
+++ minitalk/src/client.c
@@ -6,15 +6,15 @@
 /*   By: vnicoles <vnicoles@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/19 20:04:14 by vnicoles          #+#    #+#             */
-/*   Updated: 2024/12/20 12:32:38 by vnicoles         ###   ########.fr       */
+/*   Updated: 2025/01/03 05:57:28 by vnicoles         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minitalk.h"
 
-static volatile sig_atomic_t g_sig_ack;
+static volatile sig_atomic_t	g_sig_ack;
 
-void    ft_sig_handler(int signum)
+void	ft_sig_handler(int signum)
 {
 	if (signum == SIGUSR1)
 		g_sig_ack = 1;
@@ -22,46 +22,46 @@ void    ft_sig_handler(int signum)
 		ft_printf("Bogos Binted!\n");
 }
 
-void    ft_chartobits(int pid, char c)
+void	ft_chartobits(int pid, char c)
 {
-    int bit;
+	int	bit;
 
-    bit = 0;
-    while (bit < 8)
-    {
+	bit = 0;
+	while (bit < 8)
+	{
 		g_sig_ack = 0;
-        if (c & (0x01 << bit))
-            kill(pid, SIGUSR1);
-        else
-        {
-            kill(pid, SIGUSR2);
-        }
+		if (c & (0x01 << bit))
+			kill(pid, SIGUSR1);
+		else
+		{
+			kill(pid, SIGUSR2);
+		}
 		while (!g_sig_ack)
 			pause();
-        bit++;
+		bit++;
 		g_sig_ack = 0;
-    }
+	}
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    int     pid;
-    int     i;
+	int	pid;
+	int	i;
 
-    if (argc != 3)
-    {
-        ft_printf("Usage: ./client <PID> <message>\n");
-        return (1);
-    }
+	if (argc != 3)
+	{
+		ft_printf("Usage: ./client <PID> <message>\n");
+		return (1);
+	}
 	i = 0;
-    pid = ft_atoi(argv[1]);
+	pid = ft_atoi(argv[1]);
 	signal(SIGUSR1, ft_sig_handler);
 	signal(SIGUSR2, ft_sig_handler);
-    while (argv[2][i] != '\0')
-    {
-        ft_chartobits(pid, argv[2][i]);
-        i++;
-    }
-    ft_chartobits(pid, '\0');
-    return (0);
+	while (argv[2][i] != '\0')
+	{
+		ft_chartobits(pid, argv[2][i]);
+		i++;
+	}
+	ft_chartobits(pid, '\0');
+	return (0);
 }
--- minitalk/src/server.c
+++ minitalk/src/server.c
@@ -6,59 +6,103 @@
 /*   By: vnicoles <vnicoles@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/19 19:59:20 by vnicoles          #+#    #+#             */
-/*   Updated: 2024/12/20 13:12:56 by vnicoles         ###   ########.fr       */
+/*   Updated: 2025/01/03 06:22:34 by vnicoles         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minitalk.h"
 
-static void ft_handler(int sig, siginfo_t *info, void *context)
+static char	*g_msg;
+
+static void	cleanup(int sig)
 {
-    static int  bit;
-    static char msg[10000];
-    static int  i;
+	(void)sig;
+	if (g_msg)
+		free(g_msg);
+	ft_printf("\nServer shutting down...\n");
+	exit(0);
+}
+
+static char	*resize_buffer(char *old_msg, size_t old_size)
+{
+	char	*new_msg;
+
+	new_msg = ft_calloc(old_size + old_size, sizeof(char));
+	if (!new_msg)
+	{
+		free(old_msg);
+		ft_printf("Memory allocation failed in resize_buffer.\n");
+		exit(1);
+	}
+	ft_memcpy(new_msg, old_msg, old_size);
+	free(old_msg);
+	return (new_msg);
+}
+
+static int	process_message(siginfo_t *info, size_t i, size_t *capacity)
+{
+	if (g_msg[i] == '\0')
+	{
+		ft_printf("%s\n", g_msg);
+		ft_memset(g_msg, 0, 10);
+		kill(info->si_pid, SIGUSR2);
+		*capacity = 10;
+		return (0);
+	}
+	else
+		i++;
+	return (i);
+}
+
+static void	ft_handler(int sig, siginfo_t *info, void *context)
+{
+	static int		bit;
+	static size_t	i;
+	static size_t	capacity = 10;
 
 	(void)context;
-    if (sig == SIGUSR1)
-        msg[i] |= (1 << bit);
-    bit++;
-    if (bit == 8)
-    {
-        if (msg[i] == '\0')
-		{
-            ft_printf("%s\n", msg);
-			i = 0;
-			ft_memset(msg, 0, sizeof(msg));
-			kill(info->si_pid, SIGUSR2);
-		}
-		else
-        	i++;
+	if (!g_msg)
+	{
+		g_msg = ft_calloc(capacity, sizeof(char));
+		if (!g_msg)
+			exit(1);
+	}
+	if (i >= capacity - 1)
+	{
+		g_msg = resize_buffer(g_msg, capacity);
+		capacity += capacity;
+	}
+	if (sig == SIGUSR1)
+		g_msg[i] |= (1 << bit);
+	bit++;
+	if (bit == 8)
+	{
+		i = process_message(info, i, &capacity);
 		bit = 0;
-    }
+	}
 	kill(info->si_pid, SIGUSR1);
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	struct sigaction sa;
+	struct sigaction	sa;
 
-    if (argc != 1)
-    {
-        ft_printf("Usage: ./server\n");
-        return (1);
-    }
-    (void)argv;
-
+	if (argc != 1)
+	{
+		ft_printf("Usage: ./server\n");
+		return (1);
+	}
+	(void)argv;
+	ft_memset(&sa, 0, sizeof(sa));
 	sa.sa_sigaction = ft_handler;
-    sa.sa_flags = SA_SIGINFO;
-    sigaction(SIGUSR1, &sa, NULL);
-    sigaction(SIGUSR2, &sa, NULL);
-
-    ft_printf("Server PID: %d\n", getpid());
-    while (argc == 1)
-    {
-        pause();
-    }
+	sa.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	signal(SIGINT, &cleanup);
+	ft_printf("Server PID: %d\n", getpid());
+	while (argc == 1)
+	{
+		pause();
+	}
 	return (0);
 }
-
