142da7bb85f2445f5f96a9814eccb201de1bb3a1
Vladimir Nicolescu <vnicoles@c2r10s7.42prague.com>
2025-01-30 02:35:51 +0100

    buggy beta boi

--- philo/inc/philo.h
+++ philo/inc/philo.h
@@ -12,10 +12,14 @@
 
 #ifndef PHILO_H
 #define PHILO_H
+#define __STDC_FORMAT_MACROS
 
+#include <inttypes.h>
 #include <limits.h>
 #include <pthread.h>
 #include <stdalign.h>
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -23,17 +27,30 @@
 
 #define EXPECTED_ARGS 5
 
+typedef struct s_philo {
+  int id;
+  pthread_t thread;
+  pthread_mutex_t *left_fork;
+  pthread_mutex_t *right_fork;
+  uint64_t last_eaten;
+  pthread_mutex_t last_eaten_mutex;
+  struct s_philo_data *data;
+} t_philo;
+
 typedef struct s_philo_data {
   unsigned int num_philo;
-  unsigned int num_forks;
   unsigned int time_to_die;
   unsigned int time_to_eat;
   unsigned int time_to_sleep;
-  pthread_mutex_t mutex;
-  unsigned int dummy_sum;
+  pthread_mutex_t *forks;
+  t_philo *philos;
+  pthread_mutex_t printf_mutex;
+  uint64_t start_time;
+  bool sim_end;
+  pthread_mutex_t sim_end_mutex;
 } t_philo_data;
 
-void exit_with_error(const char *message, int exit_code);
 unsigned int ft_atoui(const char *num);
+void exit_with_error(const char *message, int exit_code);
 
 #endif
--- philo/src/main.c
+++ philo/src/main.c
@@ -11,7 +11,76 @@
 /* ************************************************************************** */
 
 #include "../inc/philo.h"
-#include <pthread.h>
+
+uint64_t get_current_time() {
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    return ts.tv_sec * 1000 + ts.tv_nsec / 1e6;
+}
+
+uint64_t get_curr_sim_time(void *arg)
+{
+    t_philo_data    *data;
+
+    data = (t_philo_data *)arg;
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    uint64_t now = (ts.tv_sec * 1000) + (ts.tv_nsec / 1e6);
+    return (now - data->start_time);
+}
+
+void init_sim_time(void *arg)
+{
+    t_philo_data    *data;
+
+    data = (t_philo_data *)arg;
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    data->start_time = (ts.tv_sec * 1000) + (ts.tv_nsec / 1e6);
+}
+
+void    *overseer(void *arg)
+{
+    t_philo_data    *data;
+    uint64_t        delta_time;
+    uint64_t        curr_time;
+    unsigned int    i;
+
+    data = (t_philo_data *)arg;
+    i = 0;
+    while (1)
+    {
+        pthread_mutex_lock(&data->sim_end_mutex);
+        if (data->sim_end) {
+            pthread_mutex_unlock(&data->sim_end_mutex);
+            break; // Exit the overseer thread
+        }
+        pthread_mutex_unlock(&data->sim_end_mutex);
+        while (i < data->num_philo)
+        {
+            curr_time = get_current_time();
+            printf("\ncurrent time: %"PRIu64"\nlast spaghett: %"PRIu64"\n", curr_time, data->philos[i].last_eaten);
+            pthread_mutex_lock(&data->philos[i].last_eaten_mutex);
+            delta_time = curr_time - data->philos[i].last_eaten;
+            pthread_mutex_unlock(&data->philos[i].last_eaten_mutex);
+            if (delta_time > data->time_to_die * 1000)
+            {
+                pthread_mutex_lock(&data->printf_mutex);
+                printf("%"PRIu64" Philos %u dieded\n", curr_time, data->philos[i].id);
+                pthread_mutex_unlock(&data->printf_mutex);
+                pthread_mutex_lock(&data->sim_end_mutex);
+                data->sim_end = true;
+                pthread_mutex_unlock(&data->sim_end_mutex);
+                break;
+                return NULL;
+            }
+            usleep(10000);
+            i++;
+        }
+        i = 0;
+    }
+    return NULL;
+}
 
 static void check_args(char **argv)
 {
@@ -38,67 +107,129 @@ static void check_args(char **argv)
 
 static t_philo_data  *init_philo_data(char **argv)
 {
-    t_philo_data    *philo_data = NULL;
+    unsigned int    i;
 
-    philo_data = (t_philo_data *)malloc(sizeof(t_philo_data));
-    if (!philo_data)
+    i = 0;
+    t_philo_data    *data = NULL;
+
+    data = (t_philo_data *)malloc(sizeof(t_philo_data));
+    if (!data)
         exit_with_error("Malloc failed", 1);
-    philo_data->num_philo = ft_atoui(argv[1]);
-    philo_data->num_forks = ft_atoui(argv[1]);
-    philo_data->time_to_die = ft_atoui(argv[2]);
-    philo_data->time_to_eat = ft_atoui(argv[3]);
-    philo_data->time_to_sleep = ft_atoui(argv[4]);
-    philo_data->dummy_sum = 0;
-    pthread_mutex_init(&philo_data->mutex, NULL);
-    return (philo_data);
+    data->num_philo = ft_atoui(argv[1]);
+    data->time_to_die = ft_atoui(argv[2]);
+    data->time_to_eat = ft_atoui(argv[3]);
+    data->time_to_sleep = ft_atoui(argv[4]);
+    data->sim_end = false;
+    pthread_mutex_init(&data->printf_mutex, NULL);
+    data->forks = malloc(sizeof(pthread_mutex_t) * data->num_philo);
+    while (i < data->num_philo)
+    {
+        pthread_mutex_init(&data->forks[i], NULL);
+        i++;
+    }
+    i = 0;
+    data->philos = malloc(sizeof(t_philo) * data->num_philo);
+    init_sim_time(data);
+    while (i < data->num_philo)
+    {
+        data->philos[i].id = i;
+        data->philos[i].left_fork = &data->forks[i];
+        data->philos[i].right_fork = &data->forks[(i + 1) % data->num_philo];
+        data->philos[i].data = data;
+        data->philos[i].last_eaten = data->start_time;
+        pthread_mutex_init(&data->philos[i].last_eaten_mutex, NULL);
+        i++;
+    }
+    return (data);
 }
 
-void    *routine(void   *arg)
+void    *philosopher_routine(void   *arg)
 {
-    t_philo_data    *philo_data = (t_philo_data *)arg;
+    t_philo         *philo;
+    t_philo_data    *data;
 
-    for (int i = 0; i < 10000000; i++)
+    philo = (t_philo *)arg;
+    data = philo->data;
+    while (1)
     {
-        pthread_mutex_lock(&philo_data->mutex);
-        philo_data->dummy_sum++;
-        pthread_mutex_unlock(&philo_data->mutex);
+        // THINKING
+        pthread_mutex_lock(&data->printf_mutex);
+        printf("%"PRIu64" %u is thinking\n", get_curr_sim_time(data), philo->id);
+        pthread_mutex_unlock(&data->printf_mutex);
+        usleep(5000);
+        // EATING
+        if (philo->id % 2 == 0)
+        {
+            pthread_mutex_lock(philo->right_fork);
+            pthread_mutex_lock(philo->left_fork);
+        }
+        else
+        {
+            pthread_mutex_lock(philo->left_fork);
+            pthread_mutex_lock(philo->right_fork);
+        }
+        pthread_mutex_lock(&data->printf_mutex);
+        printf("%"PRIu64" %u has taken forks %u and %u\n", get_curr_sim_time(data), philo->id, philo->id, (philo->id + 1));
+        printf("%"PRIu64" %u is eating\n", get_curr_sim_time(data), philo->id);
+        pthread_mutex_unlock(&data->printf_mutex);
+        pthread_mutex_lock(&philo->last_eaten_mutex);
+        philo->last_eaten = get_curr_sim_time(data);
+        pthread_mutex_unlock(&philo->last_eaten_mutex);
+        usleep(data->time_to_eat * 1000);
+        pthread_mutex_unlock(philo->left_fork);
+        pthread_mutex_unlock(philo->right_fork);
+        // SLEEPING
+        pthread_mutex_lock(&data->printf_mutex);
+        printf("%"PRIu64" %u is sleeping\n", get_curr_sim_time(data), philo->id);
+        pthread_mutex_unlock(&data->printf_mutex);
+        usleep(data->time_to_sleep * 1000);
+
+        pthread_mutex_lock(&data->sim_end_mutex);
+        if (data->sim_end)
+        {
+            pthread_mutex_lock(&data->sim_end_mutex);
+            break;
+        }
+        pthread_mutex_unlock(&data->sim_end_mutex);
     }
     return NULL;
 }
 
 int	main(int argc, char **argv)
 {
-    pthread_t       *philo_threads;
-    t_philo_data    *philo_data;
+    pthread_t       overseer_thread;
+    t_philo_data    *data;
     unsigned int    i;
 
     i = 0;
     if (argc != 5)
-        return(write(1, "Usage: ./philo <num_philo> <time_to_die_ms> <time_to_eat_ms> <time_to_sleep_ms>\n", 71), 1);
+        return(write(1, "Usage: ./philo <num_philo> <time_to_die_ms> <time_to_eat_ms> <time_to_sleep_ms>\n", 80), 1);
     check_args(argv);
-    philo_data = init_philo_data(argv);
-    if (!(philo_threads = malloc(sizeof(pthread_t) * philo_data->num_philo)))
-        exit_with_error("Memory allocation failed", 1);
-    while (i < philo_data->num_philo)
+    data = init_philo_data(argv);
+    while (i < data->num_philo)
     {
-        if (pthread_create(&philo_threads[i], NULL, &routine, philo_data) != 0)
+        if (pthread_create(&data->philos[i].thread, NULL, &philosopher_routine, &data->philos[i]) != 0)
             exit_with_error("Failed to create thread", 1);
-        printf("Philosopher %u has started counting...\n", i);
         i++;
     }
+    pthread_create(&overseer_thread, NULL, overseer, data);
     i = 0;
-    while (i < philo_data->num_philo)
+    while (i < data->num_philo)
     {
-        if (pthread_join(philo_threads[i], NULL) != 0)
+        if (pthread_join(data->philos[i].thread, NULL) != 0)
             exit_with_error("Failed to join thread", 1);
-        printf("Philosopher %u has finished counting!\n", i);
         i++;
     }
-    printf("End sum: %u\n", philo_data->dummy_sum);
-
-    pthread_mutex_destroy(&philo_data->mutex);
-    free(philo_data);
-    free(philo_threads);
+    pthread_join(overseer_thread, NULL);
+    pthread_mutex_destroy(&data->sim_end_mutex);
+    pthread_mutex_destroy(&data->printf_mutex);
+    for (unsigned int i = 0; i < data->num_philo; i++) {
+        pthread_mutex_destroy(&data->forks[i]);
+        pthread_mutex_destroy(&data->philos[i].last_eaten_mutex);
+    }
+    free(data->forks);
+    free(data->philos);
+    free(data);
 	return (0);
 }
 
