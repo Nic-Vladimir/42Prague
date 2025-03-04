33e16779a92d373e88e82e156dbed2dbf75fd17f
Nic-Vladimir <vladimirnicolescu2000@gmail.com>
2025-03-04 16:37:08 +0200

    add memory arena and AST

--- minishell/inc/minishell.h
+++ minishell/inc/minishell.h
@@ -6,11 +6,49 @@
 /*   By: vnicoles <vnicoles@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 20:48:22 by vnicoles          #+#    #+#             */
-/*   Updated: 2025/03/03 20:48:35 by vnicoles         ###   ########.fr       */
+/*   Updated: 2025/03/04 16:21:23 by vnicoles         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+typedef enum e_node_type {
+	NODE_CMD,
+	NODE_PIPE,
+	NODE_REDIR_IN,
+	NODE_REDIR_OUT,
+	NODE_REDIR_APPEND,
+	NODE_HEREDOC,
+	NODE_AND,
+	NODE_OR,
+	NODE_GROUP,
+} t_node_type;
+
+typedef struct s_ast_node {
+	t_node_type type;
+	char **args;
+	struct s_ast_node *left;
+	struct s_ast_node *right;
+} t_ast_node;
+
+typedef struct s_arena {
+	char	*mem;
+	size_t	size;
+	size_t	used;
+} t_arena;
+
+t_arena	*arena_init(size_t size);
+static int	arena_expand(t_arena *arena);
+void	*arena_malloc(t_arena *arena, size_t size);
+void	arena_reset(t_arena *arena);
+void	arena_free(t_arena *arena);
+
+
+#endif
 
 
---
+++ minishell/src/ast.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ast.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vnicoles <vnicoles@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/04 16:02:13 by vnicoles          #+#    #+#             */
+/*   Updated: 2025/03/04 16:36:38 by vnicoles         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/minishell.h"
+
+t_ast_node *ast_new_node(t_arena *arena, t_node_type type, char **args) {
+	t_ast_node	*node;
+
+	node = (t_ast_node *)arena_malloc(arena, sizeof(t_ast_node));
+    if (!node)
+        return NULL;
+    node->type = type;
+    node->args = args;
+	node->left = NULL;
+	node->right = NULL;
+    return node;
+}
+
+t_ast_node *ast_node_insert(t_arena *arena, t_ast_node *root, t_node_type type, char **args) {
+	if (!root)
+		return ast_new_node(arena, type, args);
+	if (type == NODE_PIPE || type == NODE_AND || type == NODE_OR) {
+		t_ast_node *new_root = ast_new_node(arena, type, NULL);
+		new_root->left = root;
+		new_root->right = ast_new_node(arena, NODE_CMD, args);
+		return new_root;
+	}
+	root->right = ast_node_insert(arena, root->right, type, args);
+	return root;
+}
+
+void	ast_execute(t_ast_node *node) {
+	if (!node)
+		return;
+	ast_execute(node->left);
+	printf("Executing: %s\n", node->args ? node->args[0] : "[OPERATOR]");
+	ast_execute(node->right);
+}
